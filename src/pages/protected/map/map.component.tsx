7389be76bf7bdc81210529b842da57fa4ee3bb41
Xileny <xileny.seijasp@gmail.com>
2023-05-15 17:22:34 +0200

    polling does not remove selected layer, configs ref #99

--- src/pages/protected/map/map.component.tsx
+++ src/pages/protected/map/map.component.tsx
@@ -445,14 +445,7 @@ export function Map() {
   /**
    * when filters are updated then update the map features to show
    */
-  const updateMapFeatures = useCallback(() => {
-    setLayerSelection({
-      isMapRequest: NO_LAYER_SELECTED,
-      mapRequestCode: NO_LAYER_SELECTED,
-      dataTypeId: NO_LAYER_SELECTED,
-      multipleLayersAllowed: false,
-      layerClicked: null
-    })    
+  const updateMapFeatures = useCallback(() => {      
     //when filters are applied use the ids[] of the selected teams in the fetchGeoJson call
     let f: any = filtersObj?.filters?.persons
     var arrayOfTeams: number[] | undefined = undefined
@@ -471,7 +464,23 @@ export function Map() {
       //if teams selections is empty reset arrayofteams to the default state (undefined)
       if (arrayOfTeams.length === 0) arrayOfTeams = undefined
     }
-    fetchGeoJson(arrayOfTeams)
+    fetchGeoJson(arrayOfTeams)    
+    const newFilterList = getFilterList(filtersObj)
+    setFilterList(newFilterList)
+    if (!toggleSideDrawer) {
+      setToggleActiveFilterTab(false)
+    }
+    forceUpdate()
+  }, [fetchGeoJson, handleGetLayersCall, layersApiFactory])
+
+  useEffect(() => {
+    setLayerSelection({
+      isMapRequest: NO_LAYER_SELECTED,
+      mapRequestCode: NO_LAYER_SELECTED,
+      dataTypeId: NO_LAYER_SELECTED,
+      multipleLayersAllowed: false,
+      layerClicked: null
+    })  
     handleGetLayersCall(() => {
       return layersApiFactory.layersGetLayers(
         undefined,
@@ -487,16 +496,7 @@ export function Map() {
         }
       )
     })
-    const newFilterList = getFilterList(filtersObj)
-    setFilterList(newFilterList)
-    if (!toggleSideDrawer) {
-      setToggleActiveFilterTab(false)
-    }
-    forceUpdate()
-  }, [fetchGeoJson, handleGetLayersCall, layersApiFactory])
-
-  useEffect(() => {
-    updateMapFeatures()
+    updateMapFeatures()    
   }, [filtersObj, fetchGeoJson, handleGetLayersCall, layersApiFactory])
 
   useEffect(() => {
