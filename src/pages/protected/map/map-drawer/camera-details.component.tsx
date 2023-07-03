91ad56e4eed7edf3b56d4ff28d38461a1a38d1dd
Gian Marco Toso <info@gianmarcotoso.com>
2023-07-03 14:58:56 +0200

    camera thumbnails pagination

--- src/pages/protected/map/map-drawer/camera-details.component.tsx
+++ src/pages/protected/map/map-drawer/camera-details.component.tsx
@@ -15,8 +15,10 @@ import {
   Button,
   Switch,
   FormGroup,
-  FormControlLabel
+  FormControlLabel,
+  IconButton
 } from '@material-ui/core'
+import { KeyboardArrowLeft, KeyboardArrowRight, ArrowLeft, ArrowRight } from '@material-ui/icons'
 import { MeasureDto, SensorDto, StationDto, StationsApiFactory } from 'ermes-backoffice-ts-sdk'
 import useCameras from '../../../../hooks/use-cameras.hook'
 import React, { CSSProperties, useEffect, useMemo, useState } from 'react'
@@ -34,6 +36,8 @@ function getCardinalDirection(angle) {
 
 type CameraDetailsProps = {}
 
+const PAGE_SIZE = 6
+
 export function CameraDetails({}: CameraDetailsProps) {
   const { t } = useTranslation(['common', 'maps'])
   const elem = useSelector((state: AppState) => state.selectedCameraState)
@@ -46,7 +50,8 @@ export function CameraDetails({}: CameraDetailsProps) {
   const [loading, setLoading] = useState(false)
   const theme = useTheme()
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
-  const [hideMeasurementsWithoutDetections, setHideMeasurementsWithoutDetections] = useState(false)
+  const [hideMeasurementsWithoutDetections, setHideMeasurementsWithoutDetections] = useState(true)
+  const [page, setPage] = useState(0)
 
   function handleClose() {
     setSelectedSensorId(undefined)
@@ -69,6 +74,33 @@ export function CameraDetails({}: CameraDetailsProps) {
       return measurement.metadata?.detection?.fire || measurement.metadata?.detection?.smoke
     })
   }, [sensorData, hideMeasurementsWithoutDetections])
+
+  const pageItems = useMemo(() => {
+    return filteredMeasurements.slice(page * PAGE_SIZE, page * PAGE_SIZE + PAGE_SIZE)
+  }, [filteredMeasurements, page])
+
+  useEffect(() => {
+    if (selectedSensorMeasurementId === null) {
+      setSelectedSensorMeasurement(filteredMeasurements[0]?.id ?? null)
+      setPage(0)
+
+      return
+    }
+
+    const index = filteredMeasurements.findIndex((m) => m.id === selectedSensorMeasurementId)
+    if (index === -1) {
+      setSelectedSensorMeasurement(filteredMeasurements[0]?.id ?? null)
+      setPage(0)
+
+      return
+    }
+
+    const newPage = Math.floor(
+      filteredMeasurements.findIndex((m) => m.id === selectedSensorMeasurementId) / PAGE_SIZE
+    )
+
+    setPage(newPage)
+  }, [filteredMeasurements, selectedSensorMeasurementId])
 
   useEffect(() => {
     if (!elem) {
@@ -199,7 +231,7 @@ export function CameraDetails({}: CameraDetailsProps) {
             </Grid>
           </Grid>
         )}
-        {!hasMeasurements && (
+        {(!hasMeasurements || filteredMeasurements?.length === 0) && !loading && (
           <Typography variant="body2" component="h2" gutterBottom>
             {t('maps:noMeasurements')}
           </Typography>
@@ -222,69 +254,92 @@ export function CameraDetails({}: CameraDetailsProps) {
             alt={sensorData?.find((s) => s.id === selectedSensorMeasurementId)?.measure}
           />
         )}
-        {sensorData && (
-          <Tabs
-            variant="scrollable"
-            value={selectedSensorMeasurementId}
-            onChange={(_, v) => setSelectedSensorMeasurement(v)}
-          >
-            {filteredMeasurements.map((measurement: MeasureDto) => {
+        {sensorData && filteredMeasurements?.length > 0 && (
+          <div style={localStyles.thumbnailContainer}>
+            <div>
+              <IconButton disabled={page === 0} onClick={() => setPage(0)}>
+                <ArrowLeft />
+              </IconButton>
+            </div>
+            <div>
+              <IconButton disabled={page === 0} onClick={() => setPage(page - 1)}>
+                <KeyboardArrowLeft />
+              </IconButton>
+            </div>
+            {pageItems.map((measurement: MeasureDto) => {
               const hasFire = measurement.metadata?.detection?.fire
               const hasSmoke = measurement.metadata?.detection?.smoke
               const thumbnail = measurement.metadata?.thumbnail_uri ?? measurement.measure
 
+              const style = { ...localStyles.thumbnail }
+              if (selectedSensorMeasurementId === measurement.id) {
+                style.borderBottom = `3px solid ${theme.palette.secondary.main}`
+              }
+
               return (
-                <Tab
-                  key={measurement.id as any}
-                  value={measurement.id}
-                  label={
-                    <div>
-                      <div style={localStyles.smallBadgeContainer}>
-                        {hasFire && (
-                          <Chip
-                            color="primary"
-                            size="small"
-                            style={{
-                              backgroundColor: theme.palette.error.dark,
-                              borderColor: theme.palette.error.dark,
-                              color: theme.palette.error.contrastText,
-                              width: 16,
-                              height: 16
-                            }}
-                            className={classes.chipStyle}
-                          />
-                        )}
-                        {hasSmoke && (
-                          <Chip
-                            color="primary"
-                            size="small"
-                            style={{
-                              backgroundColor: theme.palette.primary.contrastText,
-                              borderColor: theme.palette.primary.dark,
-                              color: theme.palette.primary.dark,
-                              width: 16,
-                              height: 16
-                            }}
-                            className={classes.chipStyle}
-                          />
-                        )}
-                      </div>
-                      <img
-                        style={{ width: 100, height: 75, objectFit: 'cover' }}
-                        src={thumbnail}
-                        alt={measurement.measure!}
-                      />
-                      {measurement.timestamp && (
-                        <Typography variant="body2">
-                          {moment(measurement.timestamp).format('YYYY/MM/DD HH:mm')}
-                        </Typography>
+                <div key={measurement.id as any}>
+                  <div style={style} onClick={() => setSelectedSensorMeasurement(measurement.id)}>
+                    <div style={localStyles.smallBadgeContainer}>
+                      {hasFire && (
+                        <Chip
+                          color="primary"
+                          size="small"
+                          style={{
+                            backgroundColor: theme.palette.error.dark,
+                            borderColor: theme.palette.error.dark,
+                            color: theme.palette.error.contrastText,
+                            width: 16,
+                            height: 16
+                          }}
+                          className={classes.chipStyle}
+                        />
+                      )}
+                      {hasSmoke && (
+                        <Chip
+                          color="primary"
+                          size="small"
+                          style={{
+                            backgroundColor: theme.palette.primary.contrastText,
+                            borderColor: theme.palette.primary.dark,
+                            color: theme.palette.primary.dark,
+                            width: 16,
+                            height: 16
+                          }}
+                          className={classes.chipStyle}
+                        />
                       )}
                     </div>
-                  }
-                />
+                    <img
+                      style={{ width: 100, height: 75, objectFit: 'cover' }}
+                      src={thumbnail}
+                      alt={measurement.measure!}
+                    />
+                    {measurement.timestamp && (
+                      <Typography variant="body2" style={{ fontSize: '0.7rem' }}>
+                        {moment(measurement.timestamp).format('YYYY/MM/DD HH:mm')}
+                      </Typography>
+                    )}
+                  </div>
+                </div>
               )
             })}
-          </Tabs>
+            <div>
+              <IconButton
+                disabled={page === Math.ceil(filteredMeasurements.length / PAGE_SIZE) - 1}
+                onClick={() => setPage(page + 1)}
+              >
+                <KeyboardArrowRight />
+              </IconButton>
+            </div>
+            <div>
+              <IconButton
+                disabled={page === Math.ceil(filteredMeasurements.length / PAGE_SIZE) - 1}
+                onClick={() => setPage(Math.ceil(filteredMeasurements.length / PAGE_SIZE) - 1)}
+              >
+                <ArrowRight />
+              </IconButton>
+            </div>
+          </div>
         )}
       </DialogContent>
     </Dialog>
@@ -292,6 +347,26 @@ export function CameraDetails({}: CameraDetailsProps) {
 }
 
 const localStyles: Record<string, CSSProperties> = {
+  thumbnailContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflowY: 'hidden',
+    overflowX: 'auto'
+  },
+  thumbnail: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0 14px',
+    borderBottom: '3px solid transparent',
+    cursor: 'pointer',
+    position: 'relative'
+  },
+  thumbnailActive: {
+    borderBottom: '3px solid #f9a825'
+  },
   titleBar: {
     display: 'flex',
     justifyContent: 'space-between',
@@ -314,8 +389,8 @@ const localStyles: Record<string, CSSProperties> = {
     display: 'flex',
     justifyContent: 'flex-end',
     alignItems: 'center',
-    bottom: 32,
+    bottom: 20,
     left: 0,
-    right: 36
+    right: 4
   }
 }
