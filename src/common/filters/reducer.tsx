ec3cc1145257b5a3de0dd4f742881b1e282471b8
Xileny <xileny.seijasp@gmail.com>
2023-11-07 10:30:41 +0100

    fix reset to 45 days ref #173

--- src/common/filters/reducer.tsx
+++ src/common/filters/reducer.tsx
@@ -1,82 +1,80 @@
-import {_MS_PER_DAY} from '../../utils/utils.common'
+import { _MS_PER_DAY } from '../../utils/utils.common'
 
 export interface FiltersType {
-    datestart: Date
-    dateend: Date
-    languageSelect?: []
-    hazardSelect?: []
-    infoTypeSelect?: []
-    informativeSelect?: string,
-    southWest?:[number,number] | undefined,
-    northEast?:[number,number] | undefined
-
+  datestart: Date
+  dateend: Date
+  languageSelect?: []
+  hazardSelect?: []
+  infoTypeSelect?: []
+  informativeSelect?: string
+  southWest?: [number, number] | undefined
+  northEast?: [number, number] | undefined
 }
 
 export interface FiltersSocialType {
-    datestart: Date
-    dateend: Date
-    languageSelect?: string []
-    hazardSelect?: []
-    infoTypeSelect?: []
-    informativeSelect?: string,
-    southWest?:[number,number] | undefined,
-    northEast?:[number,number] | undefined
-
+  datestart: Date
+  dateend: Date
+  languageSelect?: string[]
+  hazardSelect?: []
+  infoTypeSelect?: []
+  informativeSelect?: string
+  southWest?: [number, number] | undefined
+  northEast?: [number, number] | undefined
 }
 
 const filterReducer = (currentFilters: FiltersType, action: any): FiltersType => {
-    switch (action.type) {
-        case 'START_DATE':
-            return {
-                ...currentFilters,
-                datestart: action.value
-            }
-        case 'END_DATE':
-            return {
-                ...currentFilters,
-                dateend: action.value
-            }
-        case 'DATES':
-            return {
-                ...currentFilters, 
-                datestart: action.start,
-                dateend: action.end
-            }
-        case 'LANGUAGES':
-            return {
-                ...currentFilters,
-                languageSelect: action.value
-            }
-        case 'HAZARDS':
-            return {
-                ...currentFilters,
-                hazardSelect: action.value
-            }
-        case 'INFORMATIONS':
-            return {
-                ...currentFilters,
-                infoTypeSelect: action.value
-            }
-        case 'INFORMATIVE':
-            return {
-                ...currentFilters,
-                informativeSelect: action.value
-            }
-        case 'RESET':
-            const currentDate = new Date()
-            return {
-                datestart: new Date(currentDate.valueOf() - _MS_PER_DAY * 3),
-                dateend: new Date(currentDate.valueOf() + _MS_PER_DAY * 7),
-                languageSelect: [],
-                hazardSelect: [],
-                infoTypeSelect: [],
-                informativeSelect: '',
-                southWest:undefined,
-                northEast:undefined
-            }
-        default:
-            throw new Error("Invalid action type")
-    }
+  switch (action.type) {
+    case 'START_DATE':
+      return {
+        ...currentFilters,
+        datestart: action.value
+      }
+    case 'END_DATE':
+      return {
+        ...currentFilters,
+        dateend: action.value
+      }
+    case 'DATES':
+      return {
+        ...currentFilters,
+        datestart: action.start,
+        dateend: action.end
+      }
+    case 'LANGUAGES':
+      return {
+        ...currentFilters,
+        languageSelect: action.value
+      }
+    case 'HAZARDS':
+      return {
+        ...currentFilters,
+        hazardSelect: action.value
+      }
+    case 'INFORMATIONS':
+      return {
+        ...currentFilters,
+        infoTypeSelect: action.value
+      }
+    case 'INFORMATIVE':
+      return {
+        ...currentFilters,
+        informativeSelect: action.value
+      }
+    case 'RESET':
+      const currentDate = new Date()
+      return {
+        datestart: new Date(currentDate.valueOf() - _MS_PER_DAY * 3),
+        dateend: new Date(currentDate.valueOf() + _MS_PER_DAY * 45),
+        languageSelect: [],
+        hazardSelect: [],
+        infoTypeSelect: [],
+        informativeSelect: '',
+        southWest: undefined,
+        northEast: undefined
+      }
+    default:
+      throw new Error('Invalid action type')
+  }
 }
 
-export default filterReducer;
+export default filterReducer
