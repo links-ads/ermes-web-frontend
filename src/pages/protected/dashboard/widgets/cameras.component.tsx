c5b8e6792a50d9e1fb3c3a98de7d2e61f305f93c
Gian Marco Toso <info@gianmarcotoso.com>
2023-11-27 12:20:32 +0100

    fix cameras in dashboard

--- src/pages/protected/dashboard/widgets/cameras.component.tsx
+++ src/pages/protected/dashboard/widgets/cameras.component.tsx
@@ -4,8 +4,14 @@ import { ResponsiveBar } from '@nivo/bar'
 import { EmergencyColorMap } from '../../map/api-data/emergency.component'
 import { useTranslation } from 'react-i18next'
 
+export function Square({ color, style = {} }) {
+  return <div style={{ height: 16, width: 16, backgroundColor: color, ...style }}></div>
+}
+
 export function Cameras({ cameras }) {
   const { t } = useTranslation()
+
+  console.log(cameras)
 
   const data = useMemo(() => {
     if (!cameras) return []
@@ -30,50 +36,33 @@ export function Cameras({ cameras }) {
   return (
     <div>
       <div style={{ height: 32 * data.length }}>
-        <ResponsiveBar
-          data={data}
-          keys={['value', 'total']}
-          indexBy="id"
-          layout="horizontal"
-          margin={{ left: 100, right: 60 }}
-          padding={0.3}
-          valueScale={{ type: 'linear' }}
-          indexScale={{ type: 'band', round: true }}
-          isInteractive={false}
-          labelFormat={(d) => {
-            return (
-              <tspan x={10} fill="#000">
-                {d}
-              </tspan>
-            ) as any
-          }}
-          theme={{
-            textColor: '#ffffff',
-            tooltip: {
-              basic: {
-                color: '#000'
-              }
-            }
-          }}
-          colors={(bar) => {
-            const id = bar.id
-
-            return bar.data[`${id}Color`]
-          }}
-          axisTop={null}
-          axisRight={null}
-          axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0
-          }}
-          labelSkipWidth={12}
-          labelSkipHeight={12}
-          labelTextColor={{
-            from: 'color',
-            modifiers: [['darker', 1.6]]
-          }}
-        />
+        <table>
+          <tbody>
+            {cameras.map((camera) => {
+              return (
+                <tr key={camera.id} style={{ height: 32 }}>
+                  <td width="30%">
+                    <Typography variant="body2">
+                      {t(`labels:${camera.isOnline ? 'camera-online' : 'camera-offline'}`)}
+                    </Typography>
+                  </td>
+                  <td>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <Square
+                        style={{ marginRight: 5 }}
+                        color={camera.isOnline ? EmergencyColorMap.Station : '#747474'}
+                      />
+                      <Typography variant="body2">
+                        {camera.name} ({Number(camera.location.longitude).toFixed(4)},{' '}
+                        {Number(camera.location.latitude).toFixed(4)})
+                      </Typography>
+                    </div>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   )
